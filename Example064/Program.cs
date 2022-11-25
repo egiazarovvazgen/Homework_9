b711fb550afa3752769c6b883c6632e8e0d2fddd
Egiazarov Vazgen <egiazarov.vazgen@icloud.com>
2022-11-25 12:29:50 +0300

    Example064

---
+++ Example064/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая задаёт значение N и выводит все натуральные числа в промежутке от N до 1
+
+Console.WriteLine("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int m = 1;
+if (n < 1) Console.WriteLine("Неверный ввод");
+else Console.WriteLine(NumbersFromNTo1(n, m));
+Console.WriteLine();
+
+int NumbersFromNTo1(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write(NumbersFromNTo1(n, m + 1) + ", ");
+    return m;
+}
